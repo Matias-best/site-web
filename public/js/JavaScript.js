bb180df9dfb66740c1eaaab75a127ae439a74496
Matias <youremail@example.com>
2025-11-07 11:43:16 +0100

    menu deroulant

--- public/js/JavaScript.js
+++ public/js/JavaScript.js
@@ -1,27 +1,29 @@
-let darkmode = localStorage.getItem('darkmode')
-const Switch = document.getElementById('switch')
+let darkmode = localStorage.getItem('darkmode');
+const Switch = document.getElementById('switch');
+const but =document.getElementById('but');
 
 const enableDarkmode = () => {
-	document.body.classList.add('darkmode')
-	localStorage.setItem('darkmode', 'active')
+	document.body.classList.add('darkmode');
+	localStorage.setItem('darkmode', 'active');
 }
 
 const disableDarkmode = () => {
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode', null)
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
 }
 
 if(darkmode === "active") enableDarkmode();
 
 
 Switch.addEventListener("click",()=>{
-	console.log("frkf,ekr fekf ")
-	localStorage.getItem('darkmode') !=='active' ? enableDarkmode() : disableDarkmode()
+	console.log("frkf,ekr fekf ");
+	localStorage.getItem('darkmode') !=='active' ? enableDarkmode() : disableDarkmode();
 })
 
+but.addEventListener("click", toggleNavMenu);
 //menu 
 
 function toggleNavMenu() {
-	 const navMenu = document.querySelector('menu');
-	 navMenu.classList.toogle('active');
+	 const navMenu = document.querySelector('.menu');
+	 navMenu.classList.toggle('active');
 }
