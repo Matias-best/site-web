e9568b6a3cf8191d18d06cc0dbeaf5e0832b4348
LEMAIRE Matias <matias.lemaire@etu.univ-amu.fr>
2025-11-11 19:39:44 +0100

    media

--- public/js/JavaScript.js
+++ public/js/JavaScript.js
@@ -1,30 +1,30 @@
-let darkmode = localStorage.getItem('darkmode');
-const Switch = document.getElementById('switch');
-const but =document.getElementById('but');
+let darkmode = localStorage.getItem('darkmode'); // recupère le mode sombre s'il est activé
+const Switch = document.getElementById('switch'); // bouton pour activer ou désactiver le mode sombre
+const but =document.getElementById('but'); // bouton pour le menu
 
-const enableDarkmode = () => {
-	document.body.classList.add('darkmode');
-	localStorage.setItem('darkmode', 'active');
+const enableDarkmode = () => { // fonction pour activer le mode sombre
+	document.body.classList.add('darkmode'); // ajoute la classe darkmode au body
+	localStorage.setItem('darkmode', 'active'); // stocke le mode sombre dans le localStorage
 }
 
-const disableDarkmode = () => {
-    document.body.classList.remove('darkmode');
-    localStorage.setItem('darkmode', null);
+const disableDarkmode = () => { // fonction pour désactiver le mode sombre
+    document.body.classList.remove('darkmode'); // retire la classe darkmode du body
+    localStorage.setItem('darkmode', null); // supprime le mode sombre du localStorage
 }
 
-if(darkmode === "active") enableDarkmode();
+if(darkmode === "active") enableDarkmode(); // si le mode sombre est activé, on l'active
 
 
-Switch.addEventListener("click",()=>{
-	localStorage.getItem('darkmode') !=='active' ? enableDarkmode() : disableDarkmode();
+Switch.addEventListener("click",()=>{ //  événement pour le bouton switch
+	localStorage.getItem('darkmode') !=='active' ? enableDarkmode() : disableDarkmode(); // active ou désactive le mode sombre en fonction de son état actuel
 })
 
-but.addEventListener("click", toggleNavMenu);
+but.addEventListener("click", toggleNavMenu); // événement pour le bouton du menu
 //menu 
 
-function toggleNavMenu() {
-	 const navMenu = document.querySelector('.menu');
-	 navMenu.classList.toggle('active');
+function toggleNavMenu() { // fonction pour afficher ou masquer le menu
+	 const navMenu = document.querySelector('.menu'); // sélectionne le menu
+	 navMenu.classList.toggle('active'); // ajoute ou retire la classe active au menu
 }
 // Script pour ajouter le référencement local 
 // Définition des données locales
